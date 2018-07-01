9b179f6947f91132b54c599ac527a92719da1943
Kaijie Chen <i@libredolphin.me>
2018-07-01 15:43:56 +0800

    new UI for deletebook

--- src/DeleteBook.java
+++ src/DeleteBook.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
-import java.util.Map.*;
 
 public class DeleteBook {
 
@@ -16,35 +15,103 @@ public class DeleteBook {
         jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         Box vBox = Box.createVerticalBox();
-        map = new HashMap<>();
 
-        for (String name : fieldName) {
-            JPanel pn = new JPanel();
-            pn.add(new JLabel(name));
-            JTextField txt = new JTextField(10);
-            map.put(name, txt);
-            pn.add(txt);
-            vBox.add(pn);
+        JPanel searchPanel = new JPanel();
+        searchPanel.add(new JLabel("关键字"));
+        searchText = new JTextField(10);
+        searchPanel.add(searchText);
+        JButton searchBtn = new JButton("查询");
+        searchBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String stext = searchText.getText();
+                bookList = (new Mysqlutil()).selectBookByAll(stext);
+                page = 0;
+                showResult();
+                jf.pack();
+            }
+        });
+        searchPanel.add(searchBtn);
+
+        JButton prevBtn = new JButton("上一页");
+        prevBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (page > 0) {
+                    page--;
+                    showResult();
+                    jf.pack();
+                }
+
+            }
+        });
+        searchPanel.add(prevBtn);
+
+        JButton nextBtn = new JButton("下一页");
+        nextBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if ((page + 1) * pageSize < bookList.size()) {
+                    page++;
+                    showResult();
+                    jf.pack();
+                }
+
+            }
+        });
+        searchPanel.add(nextBtn);
+
+        vBox.add(searchPanel);
+
+        for (int i = 0; i < pageSize; i++) {
+            JPanel resultPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            result[i] = new JCheckBox("");
+            result[i].setSelected(false);
+            result[i].setEnabled(false);
+            result[i].setVisible(false);
+            resultPanel.add(result[i]);
+            vBox.add(resultPanel);
         }
 
         JPanel submitPanel = new JPanel();
+
         JButton submitBtn = new JButton("删除");
         submitBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String id = map.get("编　号").getText();
-                int bid = Integer.parseInt(id);
 
-                // TODO
-                // System.out.println(bid);
+		boolean has_select = false;
+		boolean has_failure = false;
+                Mysqlutil utill = new Mysqlutil();
 
-                if((new Mysqlutil()).deleteBook(bid)>0){
-                    JOptionPane.showMessageDialog(null, "您已经成功删除书籍！", "删除成功", JOptionPane.INFORMATION_MESSAGE);
-                }else {
-                    JOptionPane.showMessageDialog(null, "删除书籍失败！", "删除失败", JOptionPane.ERROR_MESSAGE);
+                for (JCheckBox r : result) {
+                    if (r.isSelected()) {
+			has_select = true;
+                        String s = r.getText();
+                        String id = s.split("/", 2)[0];
+                        int bid = Integer.parseInt(id);
+
+			if ((new Mysqlutil()).deleteBook(bid) <= 0) {
+				has_failure = true;
+			}
+                    }
                 }
+
+		if (has_select) {
+                    if (has_failure) {
+		        JOptionPane.showMessageDialog(null, "部分书籍删除失败！", "删除失败", JOptionPane.ERROR_MESSAGE);
+                    } else {
+		        JOptionPane.showMessageDialog(null, "所选书籍删除成功！", "删除成功", JOptionPane.INFORMATION_MESSAGE);
+                    }
+		} else {
+                    JOptionPane.showMessageDialog(null, "请选择要删除的书籍！", "所选为空", JOptionPane.ERROR_MESSAGE);
+		}
+                refresh(jf);
             }
         });
+        submitPanel.add(submitBtn);
         JButton returnBtn = new JButton("返回");
         returnBtn.addActionListener(new ActionListener() {
             @Override
@@ -52,7 +119,6 @@ public class DeleteBook {
                 AdminMenu.run(jf, username);
             }
         });
-        submitPanel.add(submitBtn);
         submitPanel.add(returnBtn);
         vBox.add(submitPanel);
 
@@ -61,11 +127,41 @@ public class DeleteBook {
         jf.setLocationRelativeTo(null);
         jf.setVisible(true);
         jf.pack();
+	refresh(jf);
     }
 
-    private static String[] fieldName = {
-            "编　号",
-    };
+    private static void showResult() {
+        int i = 0;
 
-    private static Map<String, JTextField> map;
+        for (JCheckBox r : result) {
+            r.setSelected(false);
+            if (page * pageSize + i < bookList.size()) {
+                Books bi = bookList.get(page * pageSize + i);
+                String text = String.valueOf(bi.getBid()) + '/' + bi.getBname() + '/' + bi.getAuthor() + '/' + bi.getPublisher() + "    剩余量:" + bi.getQuantity();
+                r.setVisible(true);
+                r.setEnabled(true);
+                r.setText(text);
+                i++;
+            } else {
+                r.setVisible(false);
+                r.setEnabled(false);
+                r.setText("");
+            }
+        }
+    }
+
+    private static void refresh(JFrame jf) {
+        String stext = searchText.getText();
+        bookList = (new Mysqlutil()).selectBookByName(stext);
+        //page = 0;
+        showResult();
+        jf.pack();
+    }
+
+    private static JTextField searchText;
+    private static int page = 0;
+    private static int pageSize = 5;
+    private static JCheckBox[] result = new JCheckBox[pageSize];
+    private static ArrayList<Books> bookList = new ArrayList<>();
+
 }
